2d56d878121818d61fc669cad76b39fe8a25da4c
Jheron Edwards <91984031+jheronedwards@users.noreply.github.com>
2021-11-09 07:54:08 -0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 export const items = [
   // Photo from GaraytalentCore
   {
+    title: 'María Pedraza - Garay Talent - Representante de actores y directores',
     pointOfInterest: 80,
     backgroundColor: "#814A0E",
   },
